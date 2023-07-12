60a40df48492da6b13c4cf603c85c285e09b9afc
claire devillers <clairedevillers@hotmail.fr>
2023-07-12 15:08:31 +0200

    add db code

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-# lancer: flask --app server run --debug
+# rappel: lancer avec: flask --app server run --debug
 # ou: flask --app server run --host=0.0.0.0 --debug
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -20,17 +20,6 @@ class Client(db.Model):
 with app.app_context():
     db.create_all()
 
-@app.get('/populate')
-def create_users():
-        for i in range(1,5):
-            client = Client(
-                username=f"claire{i}",
-                email=f"clairedevillersca+{i}@gmail.com",
-            )
-            db.session.add(client)
-            db.session.commit()
-        return redirect(url_for("index"))  
-
 @app.route("/")
 def index():
     clients = db.session.execute(db.select(Client).order_by(Client.username)).scalars()
@@ -41,77 +30,13 @@ def utilisation(client=None):
     client = db.session.query(Client).get(1)
     return render_template('utilisation.html',client=client)
 
-# sample routes
-# @app.route('/2')
-# def index2():
-#     return redirect(url_for('bad_login'))
-
-# @app.route('/badlogin')
-# def bad_login():
-#     abort(401)
-#     this_is_never_executed()
-
-# @app.route('/hello/')
-# @app.route('/hello/<name>')
-# def hello(name=None):
-#     return render_template('hello.html', name=name)
-
-# @app.route('/user/<username>')
-# def show_user_profile(username):
-#     # show the user profile for that user
-#     return f'User {escape(username)}'
-
-# @app.route('/post/<int:post_id>')
-# def show_post(post_id):
-#     # show the post with the given id, the id is an integer
-#     return f'Post {post_id}'
-
-# @app.route('/path/<path:subpath>')
-# def show_subpath(subpath):
-#     # show the subpath after /path/
-#     return f'Subpath {escape(subpath)}'
-
-# @app.route("/me")
-# def me_api():
-#     user = {"username":"claire","theme":"","image":""}
-#     return {
-#         "username": user["username"],
-#         "theme": user["theme"],
-#         #"image": url_for("user_image", filename=user.image),
-#     }
-
-
-# @app.route("/users")
-# def user_list():
-#     users = db.session.execute(db.select(User).order_by(User.username)).scalars()
-#     for user in users:
-#          print(user.username)
-#     return "hello welcome to wpo.eu"
-    #return users
-    #return [user.to_json() for user in users]
-
-# app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-
-# @app.route('/')
-# def index():
-#     if 'username' in session:
-#         return f'Logged in as {session["username"]}'
-#     return 'You are not logged in'
-
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if request.method == 'POST':
-#         session['username'] = request.form['username']
-#         return redirect(url_for('index'))
-#     return '''
-#         <form method="post">
-#             <p><input type=text name=username>
-#             <p><input type=submit value=Login>
-#         </form>
-#     '''
-
-# @app.route('/logout')
-# def logout():
-#     # remove the username from the session if it's there
-#     session.pop('username', None)
-#     return redirect(url_for('index'))
+@app.get('/populate')
+def create_users_in_db():
+        for i in range(1,5):
+            client = Client(
+                username=f"claire{i}",
+                email=f"clairedevillersca+{i}@gmail.com",
+            )
+            db.session.add(client)
+            db.session.commit()
+        return redirect(url_for("index"))  
